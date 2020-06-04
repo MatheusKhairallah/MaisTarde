35725c00c2faedb537f642b0b007505cacd8fbcd
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:53:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            DateTime agora=DateTime.Now;
+            TimeSpan MaisTarde= new TimeSpan(6, 30, 00);
+            Console.Write($"Agora é: {agora}, Mais tarde é: ");
+            Console.WriteLine(agora + MaisTarde);
+            
+            
+            
+        }
+    }
+}
